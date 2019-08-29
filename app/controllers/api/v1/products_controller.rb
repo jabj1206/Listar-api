63bf69d06435731d759f8e2f0e9de0da851242d6
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 19:15:32 -0500

    listo

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,25 +1,39 @@
-module Api
-  module V1
-    class ProductsController < ApplicationController
-      def index
-        respond = Product.all
-        render json: respond
-      end
-      def create
-        @product = Product.create(product_params)   
-        if @product.save
-          render json: { product: @product }, status: :created
-        else
-          render json: { errors: @product.errors }, status: 422
-        end
-      end
-      
+class Api::V1::ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
 
-      private 
+  def index
+    @product = Product.all
+    render json: @product
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      render json: { product: @product }, status: :created
+    else
+      render json: { errors: @product.errors }, status: 422
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product, status: 200
+    else
+      render json: { errors: @product.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    render json: @product, status: 204 if @product.destroy
+
+    head :no_content
+  end
+
+  private
 
   def product_params
     params.require(:product).permit(:name, :price)
-  end
-    end
   end
 end
