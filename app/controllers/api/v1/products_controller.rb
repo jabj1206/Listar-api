6a0e58f4f08b930ead2b2e02848e9e7658f474f0
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 19:06:52 -0500

    routes

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,11 +1,10 @@
 module Api
-    module V1
-      class Api::V1::ProductsController < ApplicationController
-        def index
-          respond = Product.all
-          render json: respond
-        end
+  module V1
+    class ProductsController < ApplicationController
+      def index
+        respond = Product.all
+        render json: respond
       end
     end
-    end
-  
+  end
+end
