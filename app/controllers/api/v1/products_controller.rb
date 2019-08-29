29c384cfe1a0460838072f995565b22443ca074e
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 19:13:24 -0500

    create

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,6 +5,21 @@ module Api
         respond = Product.all
         render json: respond
       end
+      def create
+        @product = Product.create(product_params)   
+        if @product.save
+          render json: { product: @product }, status: :created
+        else
+          render json: { errors: @product.errors }, status: 422
+        end
+      end
+      
+
+      private 
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
     end
   end
 end
