ef099f63f978fa918cc75ad29aa8e0f96f05c9e2
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 19:18:22 -0500

    ok

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,39 +1,82 @@
-class Api::V1::ProductsController < ApplicationController
-  protect_from_forgery with: :null_session
+# class Api::V1::ProductsController < ApplicationController
+#   protect_from_forgery with: :null_session
 
-  def index
-    @product = Product.all
-    render json: @product
-  end
+#   def index
+#     @product = Product.all
+#     render json: @product
+#   end
 
-  def create
-    @product = Product.create(product_params)
-    if @product.save
-      render json: { product: @product }, status: :created
-    else
-      render json: { errors: @product.errors }, status: 422
+#   def create
+#     @product = Product.create(product_params)
+#     if @product.save
+#       render json: { product: @product }, status: :created
+#     else
+#       render json: { errors: @product.errors }, status: 422
+#     end
+#   end
+
+#   def update
+#     @product = Product.find(params[:id])
+#     if @product.update(product_params)
+#       render json: @product, status: 200
+#     else
+#       render json: { errors: @product.errors }, status: 422
+#     end
+#   end
+
+#   def destroy
+#     @product = Product.find(params[:id])
+#     render json: @product, status: 204 if @product.destroy
+
+#     head :no_content
+#   end
+
+#   private
+
+#   def product_params
+#     params.require(:product).permit(:name, :price)
+#   end
+# end
+
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      protect_from_forgery with: :null_session
+      def index
+        @products = Product.all
+        render json: @products
+        end
+
+      def create
+        product = Product.new(product_params)
+        if product.save
+          render json: product
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+        end
+
+      def update
+        product = Product.find(params[:id])
+        if product.update(product_params)
+          render json: product, status: 200
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+        end
+
+      def destroy
+        product = Product.find(params[:id])
+        product.destroy
+
+        head :no_content
+        end
+
+      private
+
+      def product_params
+        params.require(:product).permit(:name, :price)
+        end
     end
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      render json: @product, status: 200
-    else
-      render json: { errors: @product.errors }, status: 422
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-    render json: @product, status: 204 if @product.destroy
-
-    head :no_content
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :price)
   end
 end
